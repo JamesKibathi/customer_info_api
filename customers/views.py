25fd75197b5a23945a3eeae683740ab5cd22006a
James Njenga <knjenga94@gmail.com>
2023-11-09 20:33:39 +0300

    Create Category,Location,Business and Customer views

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Location, Business, Customer
+from .serializers import CategorySerializer, LocationSerializer, BusinessSerializer, CustomerSerializer
 
 # Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class BusinessViewSet(viewsets.ModelViewSet):
+    queryset = Business.objects.all()
+    serializer_class = BusinessSerializer
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
